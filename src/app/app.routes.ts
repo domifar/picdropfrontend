14ebacd228f4dbb0b6a3c6cfcfde06a3267025c4
domifar <135108965+domifar@users.noreply.github.com>
2025-03-28 16:40:29 +0100

    najo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {WorkspaceComponent} from './workspace/workspace.component';
+import {VouchersComponent} from './vouchers/vouchers.component';
+import {TimeComponent} from './time/time.component';
+import {LoginComponent} from './login/login.component';
+import {LayoutComponent} from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'site/workspace', component: WorkspaceComponent},
+  {path:'site/vouchers', component: VouchersComponent},
+  {path:'site/time', component: TimeComponent},
+  {path:'site', component: LayoutComponent},
+  {path:'', component: LoginComponent},
+];
