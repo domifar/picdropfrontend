02f8afc4ea4ea54334371fbdef3e3659b4726dca
domifar <135108965+domifar@users.noreply.github.com>
2025-05-16 13:24:06 +0200

    lore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,48 @@ import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import {provideHttpClient} from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {
+  AutoRefreshTokenService, createInterceptorCondition,
+  INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG, IncludeBearerTokenCondition,
+  provideKeycloak,
+  UserActivityService,
+  withAutoRefreshToken
+} from 'keycloak-angular';
+
+const urlCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
+  urlPattern: /.*/
+})
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimationsAsync(), provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers: [provideAnimationsAsync(), provideHttpClient(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
+    provideServiceWorker('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
+    provideKeycloak({
+      config:{
+        url:'http://localhost:8080/',
+        realm:'htl',
+        clientId: 'public'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+      },
+      features:[
+        withAutoRefreshToken({
+          onInactivityTimeout: 'logout',
+          sessionTimeout: 60000
+        })
+      ],
+      providers:[AutoRefreshTokenService, UserActivityService]
+    }),
+    {
+      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+      useValue: [urlCondition]
+    },
+  ]
 };
